e846ad598a71a2b3f7b4a24dd6643532531a576a
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-02 11:14:23 -0600

    Create robot.js

---
+++ rv/robot.js
@@ -0,0 +1,31 @@
+
+function setup(){
+ var esferaForma=new THREE.SphereGeometry(1);
+ var cilindroForma=new THREE.CylinderGeometry(0.5,0.5,4);
+ cilindro.matrix.makeRotationX(Math.PI/2);
+ var esfera1=new THREE.Mesh(esferaForma);
+ var esfera2=new THREE.Mesh(esferaForma);
+ var cilindro=new THREE.Mesh(cilindroForma);
+ esfera1.position.y=2;
+ esfera2.position.y=-2;
+ var forma=new THREE.Geometry();
+ THREE.GeometryUtils.merge(forma,esfera1);
+ malla=new THREE.Mesh(forma);
+ wireframe = new THREE.WireframeHelper( malla, 0x00ff00 );
+ escena=new THREE.Scene();
+ escena.add(malla,wireframe);
+ camara=new THREE.PerspectiveCamera();
+ camara.position.z=10;
+ renderer=new THREE.WebGLRenderer();
+ renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+ document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+requestAnimationFrame(loop);
+renderer.render(escena,camara);
+}
+
+var escena,camara,renderer,malla,wireframe;
+setup();
+loop();
