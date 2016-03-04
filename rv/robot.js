cf9fe45a08c5c94d02cef3e3d9a339f50e7ed8db
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-03 21:29:29 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -2,12 +2,12 @@
 function setup(){
  var cilindroForma=new THREE.CylinderGeometry(0.5,0.5,4);
  var cilindro=new THREE.Mesh(cilindroForma);
- var rotation = new THREE.Matrix4().makeRotationY(Math.PI/2);
+ var rotation = new THREE.Matrix4().makeRotationX(Math.PI/2);
  cilindro.applyMatrix(rotation);
  var forma=new THREE.Geometry();
  THREE.GeometryUtils.merge(forma,cilindro);
  malla=new THREE.Mesh(forma);
- wireframe = new THREE.WireframeHelper( malla, 0x00ff00 );
+ wireframe = new THREE.WireframeHelper( malla, 0x0000ff );
  escena=new THREE.Scene();
  escena.add(malla,wireframe);
  camara=new THREE.PerspectiveCamera();
