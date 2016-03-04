fc90016740e4e2bf687f8a9c7036c12841d89b70
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-03 23:18:48 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -1,6 +1,9 @@
 function setup(){
  var llantaForma=new THREE.CylinderGeometry(0.5,0.5,1);
  var material=new THREE.MeshBasicMaterial({color:0x0f0f0f});
+ var material=new THREE.MeshBasicMaterial({color:0xf0f0f0});
+ var placaForma=new THREE.BoxGeometry(6,0.5,4);
+ var placa=new THREE.Mesh(placaForma,material2);
  var llanta1=new THREE.Mesh(llantaForma,material);
  var llanta2=new THREE.Mesh(llantaForma,material);
  var llanta3=new THREE.Mesh(llantaForma,material);
@@ -11,25 +14,25 @@ function setup(){
  llanta1.applyMatrix(rotation);
  llanta1.position.x=-2;
  llanta1.position.z=2;
- 
+ llanta1.position.y=-3;
  llanta2.applyMatrix(rotation);
  llanta2.position.z=2;
- 
+ llanta2.position.y=-3;
  llanta3.applyMatrix(rotation);
  llanta3.position.x=2;
  llanta3.position.z=2;
-
+ llanta3.position.y=-3;
  llanta4.applyMatrix(rotation);
  llanta4.position.x=-2;
  llanta4.position.z=-2;
-
+ llanta4.position.y=-3;
  llanta5.applyMatrix(rotation);
  llanta5.position.z=-2;
-
+ llanta5.position.y=-3;
  llanta6.applyMatrix(rotation);
  llanta6.position.x=2;
  llanta6.position.z=-2;
-
+ llanta6.position.y=-3;
  wllanta1 = new THREE.WireframeHelper( llanta1, 0x7f0000 );
  wllanta2 = new THREE.WireframeHelper( llanta2, 0x7f0000 );
  wllanta3 = new THREE.WireframeHelper( llanta3, 0x7f0000 );
@@ -37,7 +40,7 @@ function setup(){
  wllanta5 = new THREE.WireframeHelper( llanta5, 0x7f0000 );
  wllanta6 = new THREE.WireframeHelper( llanta6, 0x7f0000 );
  escena=new THREE.Scene();
- escena.add(llanta1,wllanta1,llanta2,wllanta2,llanta3,wllanta3,llanta4,wllanta4,llanta5,wllanta5,llanta6,wllanta6);
+ escena.add(llanta1,wllanta1,llanta2,wllanta2,llanta3,wllanta3,llanta4,wllanta4,llanta5,wllanta5,llanta6,wllanta6,placa);
  camara=new THREE.PerspectiveCamera();
  camara.position.z=10;
  camara.position.y=6;
