e6f3d397989bc0a4ef338f5b2c7247352e4e36f0
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-04 00:14:09 -0600

    Update robot.js

--- rv/robot.js
+++ rv/robot.js
@@ -60,7 +60,7 @@ function setup(){
  camara.position.z=10;
  camara.position.y=6;
  renderer=new THREE.WebGLRenderer();
- renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+ renderer.setSize(window.innerHeight*.95,window.innerWidth*.95);
  document.body.appendChild(renderer.domElement);
 }
 
