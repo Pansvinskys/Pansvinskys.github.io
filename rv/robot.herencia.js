2a2ac3b39fc0d4fd52bcf88fdcd5a59db6f0368e
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-09 22:42:11 -0600

    Update robot.herencia.js

--- rv/robot.herencia.js
+++ rv/robot.herencia.js
@@ -1,4 +1,4 @@
-function llanta(xp,yp,zp,angx,angy,angz){
+function Llanta(xp,yp,zp,angx,angy,angz){
  THREE.Object3D.call(this);
  this.llanta=new THREE.Mesh(new THREE.CylinderGeometry(0.5,0.5,1),new THREE.MeshBasicMaterial({color:0x0f0f0f}));
  this.llanta.position.x=xp;
@@ -15,7 +15,7 @@ function llanta(xp,yp,zp,angx,angy,angz){
  this.add(this.wllanta);
 }
 
-function cuerpo(){
+function Cuerpo(){
  THREE.Object3D.call(this);
  this.placa=new THREE.Mesh(new THREE.BoxGeometry(6,0.5,4),new THREE.MeshBasicMaterial({color:0xb0b0b0}));
  this.caja=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshBasicMaterial({color:0xb0b0b0}));
@@ -30,25 +30,25 @@ function cuerpo(){
  this.add(this.cam);
 }
 
-function robot(){
- llanta.call(this);
- cuerpo.call(this);
- this.llanta1=new llanta(-2,2,-3,Math.PI/2,0,0);
- this.llanta2=new llanta(0,-3,2,Math.PI/2,0,0);
- this.llanta3=new llanta(2,2,-3,Math.PI/2,0,0);
- this.llanta4=new llanta(-2,-2,-3,Math.PI/2,0,0);
- this.llanta5=new llanta(0,-3,-2,Math.PI/2,0,0);
- this.llanta6=new llanta(2,-3,-2,Math.PI/2,0,0);
- this.cuerpobot=new cuerpo();
+function Robot(){
+ Llanta.call(this);
+ Cuerpo.call(this);
+ this.llanta1=new Llanta(-2,2,-3,Math.PI/2,0,0);
+ this.llanta2=new Llanta(0,-3,2,Math.PI/2,0,0);
+ this.llanta3=new Llanta(2,2,-3,Math.PI/2,0,0);
+ this.llanta4=new Llanta(-2,-2,-3,Math.PI/2,0,0);
+ this.llanta5=new Llanta(0,-3,-2,Math.PI/2,0,0);
+ this.llanta6=new Llanta(2,-3,-2,Math.PI/2,0,0);
+ this.cuerpobot=new Cuerpo();
  this.wrobot=new THREE.WireframeHelper(this.cuerpobot,0x0);
  this.add(this.llanta1,this.llanta2,this.llanta3,this.llanta4,this.llanta5,this.llanta6,this.cuerpobot,this.wrobot);
 }
 
-llanta.prototype=new THREE.Object3D();
-cuerpo.prototype=new THREE.Object3D();
+Llanta.prototype=new THREE.Object3D();
+Cuerpo.prototype=new THREE.Object3D();
 
 function setup(){
- rob=new robot();
+ rob=new Robot();
  escena=new THREE.Scene();
  escena.add(rob);
  camara=new THREE.PerspectiveCamera();
