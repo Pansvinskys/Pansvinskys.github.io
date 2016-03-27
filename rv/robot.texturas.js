f9ae53e422bd1a2f4b552ad1959cbed345b61667
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-27 03:39:56 -0600

    Update robot.texturas.js

--- rv/robot.texturas.js
+++ rv/robot.texturas.js
@@ -18,7 +18,7 @@ function posicionLlanta(cosa,xp,yp,zp){
 
 function Cuerpo(){
  THREE.ImageUtils.crossOrigin='';
- var textura  =THREE.ImageUtils.loadTexture('textures/compressed/hepatica_dxt3_mip.dds');
+ var textura  =THREE.ImageUtils.loadTexture('https://github.com/mrdoob/three.js/blob/master/examples/textures/compressed/hepatica_dxt3_mip.dds');
  textura.anisotropy = 4;
  THREE.Object3D.call(this);
  this.placa=new THREE.Mesh(new THREE.BoxGeometry(6,0.5,4),new THREE.MeshPhongMaterial({map:textura}));
