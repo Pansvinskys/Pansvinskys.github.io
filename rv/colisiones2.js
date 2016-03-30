4f3a06b0ae7cbe1311e0f631d51eae44a85ce5ac
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-30 11:25:49 -0600

    Update colisiones2.js

--- rv/colisiones2.js
+++ rv/colisiones2.js
@@ -3,7 +3,7 @@ cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial()
 cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
 pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5),new THREE.MeshNormalMaterial());
 cubo1.position.x=7;
-cubo2.position.y=-7;
+cubo2.position.x=-7;
 camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
 raycaster1=new THREE.Raycaster(pelota.position,new THREE.Vector3(1,0,0));
@@ -11,14 +11,14 @@ raycaster2=new THREE.Raycaster(pelota.position,new THREE.Vector3(-1,0,0));
 escena=new THREE.Scene();
 escena.add(cubo1,cubo2,pelota,camara);
 renderer=new THREE.WebGLRenderer();
-renderer.setSize(window.innerHeight*.95,window.innerHeigth*.95);
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
 step=0.01;
 }
 
 function loop(){
 obstaculo1=raycaster1.intersectObject(cubo1);
-obstaculo2=raycaster1.intersectObject(cubo2);
+obstaculo2=raycaster2.intersectObject(cubo2);
 if((obstaculo1.length>0&&(obstaculo1[0].distance<=0.5))||(obstaculo1.length>0&&(obstaculo1[0].distance<=0.5)))
 step=-step;
 pelota.position.x+=step;
