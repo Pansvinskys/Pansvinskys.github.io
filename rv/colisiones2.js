10dbd519901edfc28acd131bb19e36b93cd75711
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-30 11:04:51 -0600

    Update colisiones2.js

--- rv/colisiones2.js
+++ rv/colisiones2.js
@@ -1,7 +1,7 @@
 function setup(){
 cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
 cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-pelota=new THREE.Mesh(new THREE.SphereGeometry0(0.5),new THREE.MeshNormalMaterial());
+pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5),new THREE.MeshNormalMaterial());
 cubo1.position.x=7;
 cubo2.position.y=-7;
 camara=new THREE.PerspectiveCamera();
