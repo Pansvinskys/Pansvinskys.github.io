dbf3c4181a2d88a01c9604d5f8132d1d3a9b567d
Pansvinskys <axolotl.upiita@gmail.com>
2016-03-30 10:39:32 -0600

    Update colisiones.js

--- rv/colisiones.js
+++ rv/colisiones.js
@@ -6,7 +6,7 @@ cubo2.position.x=-0.7;
 camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 raycaster=new THREE.Raycaster();
-raycaster.setFromCamera(new THREE.Vector2(0,0),camera);
+raycaster.setFromCamera(new THREE.Vector2(0,0),camara);
 escena=new THREE.Scene();
 escena.add(cubo1,cubo2,camara);
 renderer=new THREE.WebGLRenderer();
