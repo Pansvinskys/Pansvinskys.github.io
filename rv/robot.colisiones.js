0269baccc06908cb8d6ddccdabcd10d4b9e223e4
Pansvinskys <axolotl.upiita@gmail.com>
2016-04-08 13:57:05 -0500

    Update robot.colisiones.js

--- rv/robot.colisiones.js
+++ rv/robot.colisiones.js
@@ -69,9 +69,9 @@ function Obstaculos(){
  for(j=0;j<=5;j++){
   for(i=0;i<=10;i++){
    if(mapa[j][i]=="x"){
-    this.cubo1[j*10+i]=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshBasicMaterial({map:textura}));
-    this.cubo1[j*10+i].position.x=-10+i;
-    this.cubo1[j*10+i].position.z=-10+j;
+    this.cubo1[j*10+i]=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshBasicMaterial({map:textura}));
+    this.cubo1[j*10+i].position.x=-10+2*i;
+    this.cubo1[j*10+i].position.z=-10+2*j;
     this.add(this.cubo1[j*10+i]);
    }
    else if(mapa[j][i]=="0"){
