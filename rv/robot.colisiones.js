431ab17ec58ec5cc4ad0b01c0f71596d1a8348da
Pansvinskys <axolotl.upiita@gmail.com>
2016-04-09 22:38:23 -0500

    Update robot.colisiones.js

--- rv/robot.colisiones.js
+++ rv/robot.colisiones.js
@@ -113,7 +113,7 @@ function setup(){
 }
 
 function loop(){
-obstaculo1=obs.raycaster.intersectObjects(escena.children);
+obstaculo1=obs.raycaster.intersectObjects(escena.children,true);
 if((obstaculo1.length>0&&(obstaculo1[0].distance<=0.5)))
 step=-step;
 obs.rob.position.x+=step;
