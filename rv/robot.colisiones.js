3a97df9449a1fb603a90ad9f677d42e3da0bd03b
Pansvinskys <axolotl.upiita@gmail.com>
2016-04-09 23:48:27 -0500

    Update robot.colisiones.js

--- rv/robot.colisiones.js
+++ rv/robot.colisiones.js
@@ -98,9 +98,9 @@ Obstaculos.prototype=new THREE.Object3D();
 
 function setup(){
  var luzPuntual=new THREE.PointLight(0xffffff);
- luzPuntual.position.x=10;
- luzPuntual.position.y=10;
- luzPuntual.position.z=10;
+ luzPuntual.position.x=0;
+ luzPuntual.position.y=0;
+ luzPuntual.position.z=0;
  obs=new Obstaculos();
  escena=new THREE.Scene();
  escena.add(luzPuntual,obs);
