9b47562be0cbf0ec99b619b6a99708022c839ab4
Pansvinskys <axolotl.upiita@gmail.com>
2016-04-27 11:01:03 -0500

    Update robot.estructura.paredes.js

--- rv/robot.estructura.paredes.js
+++ rv/robot.estructura.paredes.js
@@ -81,7 +81,7 @@ Robots.prototype=new Agent();
 Robots.prototype.sense= function(environment){
 //this.spotLight.position.set(this.position);
 //this.spotLight.rotation.y=Math.PI/2;
-this.luzPuntual.position.set(this.position);
+this.luzPuntual.position.set(this.position.x,this.position.y,this.position.z+10);
 this.sensor.set(this.position,new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
 var obstaculo= this.sensor.intersectObjects(environment.children,true);
 if((obstaculo.length>0 && 
