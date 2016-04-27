d841236b2fcf1b362d699adc47b3e759344eba0b
Pansvinskys <axolotl.upiita@gmail.com>
2016-04-27 11:15:54 -0500

    Update robot.estructura.paredes.js

--- rv/robot.estructura.paredes.js
+++ rv/robot.estructura.paredes.js
@@ -79,7 +79,7 @@ this.add(this.actuator,this.spotLight);
 Robots.prototype=new Agent();
 
 Robots.prototype.sense= function(environment){
-this.spotLight.position.set(this.position.x,this.position.y,this.position.z+10);
+this.spotLight.position.set(this.position.x,this.position.y,this.position.z+1);
 //this.spotLight.rotation.y=Math.PI/2;
 //this.luzPuntual.position.set(this.position.x,this.position.y,this.position.z+10);
 this.sensor.set(this.position,new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
