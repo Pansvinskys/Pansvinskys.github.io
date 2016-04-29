e974ae9d03f6dd8567bf3a06972ba79c9cc395b1
Pansvinskys <axolotl.upiita@gmail.com>
2016-04-28 22:29:55 -0500

    Update robot.estructura.paredes.js

--- rv/robot.estructura.paredes.js
+++ rv/robot.estructura.paredes.js
@@ -72,7 +72,7 @@ this.actuator= new Robot();
 this.actuator.rotation.x=Math.PI/2;
 this.actuator.commands=[];
 //this.luzPuntual=new THREE.PointLight(0xFFFFFF);
-this.spotLight = new THREE.SpotLight(0xffffff,10,10,0.31);
+this.spotLight = new THREE.SpotLight(0xffffff,10,1,0.31);
 this.add(this.actuator,this.spotLight);
 }
 
