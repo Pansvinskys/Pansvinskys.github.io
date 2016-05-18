a77079264c4c60d9d888cc6d0e73f2a2b6049caf
Pansvinskys <axolotl.upiita@gmail.com>
2016-05-18 10:40:17 -0500

    Update robot.conjunto.paredes.js

--- rv/Proyecto/robot.conjunto.paredes.js
+++ rv/Proyecto/robot.conjunto.paredes.js
@@ -18,7 +18,7 @@ Environment.prototype.setMap=function(map){
  else if(map[i][j]==='r')
  this.add(new Robots(-20+4*j,-20+4*i));
  camera=new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
- camera.position.set(-20+4*j,-20+4*i,0);
+ camera.position.set(-20+4*j,-20+4*i,20);
  }
 }
 
