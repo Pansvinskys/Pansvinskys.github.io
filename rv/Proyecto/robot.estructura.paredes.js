a26d5c58e8531d1311b2005ccab9af858f153210
Pansvinskys <axolotl.upiita@gmail.com>
2016-05-18 10:45:05 -0500

    Update robot.estructura.paredes.js

--- rv/Proyecto/robot.estructura.paredes.js
+++ rv/Proyecto/robot.estructura.paredes.js
@@ -71,6 +71,8 @@ this.sensor= new Sensor();
 this.actuator= new Robot();
 this.actuator.rotation.x=Math.PI/2;
 this.actuator.commands=[];
+this.camara=new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+this.camera.position.set(1,0,0);
 //this.luzPuntual=new THREE.PointLight(0xFFFFFF);
 this.spotLight = new THREE.SpotLight(0xffffff,10);
 this.spotLight.target.position.set(1,0,0);
