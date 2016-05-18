e74dc51fdf951aa1767391be7986b5a8b319947e
Pansvinskys <axolotl.upiita@gmail.com>
2016-05-18 11:10:34 -0500

    Update robot.conjunto.paredes.js

--- rv/Proyecto/robot.conjunto.paredes.js
+++ rv/Proyecto/robot.conjunto.paredes.js
@@ -50,7 +50,7 @@ function setup(){
  environment=new Environment();
  environment.setMap(mapa);
  camara=THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
- camara.position.set(environment.getObjectById(idRobot).position);
+ camara.position.set(idRobot.position);
  renderer=new THREE.WebGLRenderer();
  renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
  document.body.appendChild(renderer.domElement);
