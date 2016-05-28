bbca350d1b6e528e0d3b3bdb04210563495cb722
Pansvinskys <axolotl.upiita@gmail.com>
2016-05-28 15:21:05 -0500

    Update robot.conjunto.paredes.js

--- rv/Proyecto/robot.conjunto.paredes.js
+++ rv/Proyecto/robot.conjunto.paredes.js
@@ -82,6 +82,7 @@ function onKeyUp ( event ) {
 function loop(){
  if(boton==true){
 	camara=new THREE.OrthographicCamera( 50/ - 2, 50 / 2, 50 / 2, 50 / - 2, 10, 100 );
+	camara.rotation.y=Math.PI/2;
 	camara.lookAt(idRobot);
 }
 else{
